a6392859161683b88c9a3da02e066282913de234
Timilehin Ogunseye <togunseye@axxess.com>
2022-07-06 12:47:50 +0100

    complete

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,7 +2,7 @@ const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://teecode-001-site4.etempurl.com/api/v1"
     : // @ts-ignore
-      window.env.baseUrl;
+    "http://teecode-001-site4.etempurl.com/api/v1";
 
 const CONFIG = {
   BASE_URL: baseUrl,
